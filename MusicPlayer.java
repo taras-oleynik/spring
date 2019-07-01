dfc1f9df304a640ae076bf464d4e359e634e4d70
Taras Oleynik <taras.oleynik@gmail.com>
2019-07-01 23:18:51 +0300

    Spring learning 1,2 43

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -2,7 +2,37 @@ package com.alishev.springcourse;
 
 public class MusicPlayer {
 
+    private int volume;
+
+
+
     private Music music;
+    private String name;
+
+    public MusicPlayer() {
+        this.music = music;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
--- TestSpring.java
+++ TestSpring.java
@@ -7,9 +7,13 @@ public class TestSpring {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        // Music music = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
 
 
@@ -19,4 +23,4 @@ public class TestSpring {
         System.out.println(testBean.getName());
         context.close();*/
     }
-    }
+}
